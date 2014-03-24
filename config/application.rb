fe38f2300d1a33494019f228f9d1b6b7109a4b2f
earnestaddae <earnestaddae@gmail.com>
2014-03-24 15:12:12 +0000

    add comment to config/app

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module LearnRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+    #to prevent precompilation of assets
     config.assets.initialize_on_precompile = false
 end
